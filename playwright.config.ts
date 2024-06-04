8c8d60a9c63da4f1c879b54017fab80bd9e5e7eb
Alfira Galliamova <alfiragalliamova@MacBook-Pro-Alfira.local>
2024-06-04 00:03:31 -0400

    assertion

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 2,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -28,9 +28,15 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    video:'on-first-retry'
   },
 
   /* Configure projects for major browsers */
+ 
+ expect:{
+timeout:5000
+ },
+ testMatch: 'test.list.ts',
   projects: [
     {
       name: 'chromium',
