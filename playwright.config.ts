a88d4c95a36aec33296bd8aa5ae1c4bfa5058440
Alfira Galliamova <alfiragalliamova@MacBook-Pro-Alfira.local>
2024-06-05 13:45:31 -0400

    fixture Q1

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 2,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -24,17 +24,23 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.BASEURL || 'http://127.0.0.1:3000',
+    // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    video:'on-first-retry'
   },
 
   /* Configure projects for major browsers */
+ 
+ expect:{
+timeout:5000
+ },
+ testMatch: 'test.list.ts',
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'], headless: false},
+      use: { ...devices['Desktop Chrome'] },
     },
 
     // {
