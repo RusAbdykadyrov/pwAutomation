537cf765dd5483b39e681ace79d4141896c9728d
Alfira Galliamova <alfiragalliamova@MacBook-Pro-Alfira.local>
2024-06-10 10:26:53 -0400

    lambdaHome test

--- tests/project/iframe.spec.ts
+++ tests/project/iframe.spec.ts
@@ -1,8 +1,8 @@
 import { expect, test } from '@playwright/test'
 test.describe('IFRAME', () => {
     test('iframe test ', async ({ page }) => {
-        await page.goto('https://www.lambdatest.com/selenium-playground/nested-frames')
-        const frameBottom = page.frameLocator('[name=frame-bottom')
+        await page.goto('https://www.lambdatest.com/selenium-playground/nested-fram')
+        const frameBottom = page.frameLocator('[name="frame-bottom"]')
         const leftFrame = await frameBottom
             .frameLocator('[name="frame-left"]')
             .locator("body")
