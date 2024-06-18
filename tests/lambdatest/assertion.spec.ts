36eebca355fd3274d431d86a0db1b9be66388a44
Alfira Galliamova <alfiragalliamova@MacBook-Pro-Alfira.local>
2024-06-18 12:45:47 -0400

    create test5 yml

--- tests/lambdatest/assertion.spec.ts
+++ tests/lambdatest/assertion.spec.ts
@@ -1,16 +1,17 @@
 import { test, expect } from '@playwright/test'
 test.describe('ASSERTION', () => {
-    //.use({viewport: { width:500,height:500}})
+    test.use({viewport: { width:500,height:500}})
     test('assertion', async ({ page }) => {
 
         await page.goto('https://www.lambdatest.com/')
-        test.use({viewport: { width:500,height:500}})
+        //test.use({
+ // viewport: { width: 1280, height: 720 },//});
         const title = await page.title()
         console.log(title)
         expect.soft(title).toContain('LambdaTest')
         const header = page.locator('h1')
         expect(await header.textContent()).toBe('Next-Generation Mobile Apps and Cross Browser Testing Cloud')
-        expect(page.locator('img[scr$="Vimeo.svg"]')).toHaveAttribute('alt', 'vimeo', {timeout: 1000})
+       // expect(page.locator('img[scr$="Vimeo.svg"]')).toHaveAttribute('alt', 'vimeo', {timeout: 1000})
 
     })
 
